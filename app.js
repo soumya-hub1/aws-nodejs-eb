5e2470965422d02d19da3c50e7d5bf3a0ad2244b
soumya-hub1 <118823679+soumya-hub1@users.noreply.github.com>
2023-07-10 23:47:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended:false}))
 
 app.get('/', function (req, res) {
   res.render('index', {
-    title: 'S3 CloudHub YT channel & Elastic Beanstalk'
+    title: 'RN Node JS code pipeline test'
     })
     res.status(200).end();
 })
